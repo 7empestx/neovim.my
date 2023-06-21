e63092d3d356fb022f0934ca84d3a7b7e1a6f8c1
Grant Starkman <grant.starkman@gmail.com>
2023-06-21 11:25:49 -0600

    typescript server

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,10 +1,23 @@
-local lsp = require('lsp-zero').preset({rust = true, typescript = true})
+local lsp = require('lsp-zero').preset()
 
 lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
 end)
 
-lsp.setup({rust = {}, typescript = {}})
+lsp.skip_server_setup({'tsserver'})
+
+lsp.setup()
+
+require('typescript').setup({
+  server = {
+    on_attach = function(client, bufnr)
+      -- You can find more commands in the documentation:
+      -- https://github.com/jose-elias-alvarez/typescript.nvim#commands
+
+      vim.keymap.set('n', '<leader>ci', '<cmd>TypescriptAddMissingImports<cr>', {buffer = bufnr})
+    end
+  }
+})
 
 -- You need to setup `cmp` after lsp-zero
 local cmp = require('cmp')
