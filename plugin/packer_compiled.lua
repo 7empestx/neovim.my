caa0efa5d94386f71123c8f3f5f851e81357b08f
Grant Starkman <gstarkma@amazon.com>
2023-06-23 18:49:21 -0600

    compiled...probably should ignore this

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -159,6 +159,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/gstarkma/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
+  },
+  ["vim-gitgutter"] = {
+    loaded = true,
+    path = "/Users/gstarkma/.local/share/nvim/site/pack/packer/start/vim-gitgutter",
+    url = "https://github.com/airblade/vim-gitgutter"
   }
 }
 
