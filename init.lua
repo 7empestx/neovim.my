bdab1a1e43951470cdf63eafeef7243d7e2121b2
Grant Starkman <grant.starkman@gmail.com>
2023-06-25 11:28:44 -0600

    windows clipboard

--- init.lua
+++ init.lua
@@ -1,1 +1,15 @@
 require("grant")
+
+vim.g.clipboard = {
+    name = 'Win32yank',
+    copy = {
+        ['+'] = 'win32yank.exe -i --crlf',
+        ['*'] = 'win32yank.exe -i --crlf',
+    },
+    paste = {
+        ['+'] = 'win32yank.exe -o --lf',
+        ['*'] = 'win32yank.exe -o --lf',
+    },
+    cache_enabled = 1,
+}
+  
