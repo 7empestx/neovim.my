7973358761b7c30ce968a623e4a8177276f31b52
Grant Starkman <grant.starkman@gmail.com>
2023-07-08 10:05:35 -0600

    remaps and more plugins

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -4,8 +4,7 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-a>", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<C-7>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-8>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-9>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-0>", function() ui.nav_file(4) end)
-
+vim.keymap.set("n", "∆", function() ui.nav_file(1) end)
+vim.keymap.set("n", "˚", function() ui.nav_file(2) end)
+vim.keymap.set("n", "¬", function() ui.nav_file(3) end)
+vim.keymap.set("n", "…", function() ui.nav_file(4) end)
