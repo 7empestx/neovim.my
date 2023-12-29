c17b7e7f0dd5992ffb0385ba939b3d58b4799f22
Grant Starkman <grant.starkman@gmail.com>
2023-12-29 14:47:11 -0700

    update telescope

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -20,7 +20,7 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_b = {'diff', 'diagnostics'},
     lualine_c = {'filename'},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
