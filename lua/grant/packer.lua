49eaee18cd291d0fd15b781d6b4f436cc2bb1c82
Grant Starkman <grant@thatch.tech>
2024-03-02 20:06:20 -0700

    Use my version of noice

--- lua/grant/packer.lua
+++ lua/grant/packer.lua
@@ -66,9 +66,8 @@ return require('packer').startup(function(use)
 
   -- Going to also try out noice
   -- Noice is noice but too buggy
-  --[[
   use {
-    'folke/noice.nvim',
+    '7empestx/noice.nvim',
     event = "VimEnter",
     config = function()
       require('noice').setup()
@@ -77,27 +76,7 @@ return require('packer').startup(function(use)
       "MunifTanjim/nui.nvim",
       "rcarriga/nvim-notify"
     },
-    lsp = {
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true,
-      },
-    },
-    presets = {
-      bottom_search = true,       -- use a classic bottom cmdline for search
-      command_palette = true,     -- position the cmdline and popupmenu together
-      long_message_to_split = true, -- long messages will be sent to a split
-      inc_rename = false,         -- enables an input dialog for inc-rename.nvim
-      lsp_doc_border = false,     -- add a border to hover docs and signature help
-    },
-    cmdline = {
-      enable = false,              -- enable cmdline mode
-      view = "cmdline",            -- the view to use for cmdline 
-
-    }
   }
-  --]]
 
   use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
   use('theprimeagen/harpoon')
