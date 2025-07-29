d703ca40f9468a0086724e4fb539561400ee6f67
Grant Starkman <gstarkman@nex.io>
2025-07-29 09:55:29 -0600

    new config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,42 +1,55 @@
-local lsp = require('lsp-zero').preset()
-
-lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
-end)
-
-lsp.skip_server_setup({'tsserver'})
-
-lsp.setup()
-
-require('typescript').setup({
-  server = {
-    on_attach = function(client, bufnr)
-      -- You can find more commands in the documentation:
-      -- https://github.com/jose-elias-alvarez/typescript.nvim#commands
-
-      vim.keymap.set('n', '<leader>ci', '<cmd>TypescriptAddMissingImports<cr>', {buffer = bufnr})
-      vim.keymap.set('n', '<leader>cr', '<cmd>TypescriptRenameFile<cr>', {buffer = bufnr})
-      vim.keymap.set('n', '<leader>cf', '<cmd>TypescriptOrganizeImports<cr>', {buffer = bufnr})
-      vim.keymap.set('n', '<leader>ca', '<cmd>TypescriptCodeAction<cr>', {buffer = bufnr})
-      vim.keymap.set('n', '<leader>ce', '<cmd>TypescriptErrors<cr>', {buffer = bufnr})
-    end
+-- Mason setup
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = { 'ts_ls' },
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup({})
+    end,
+    ts_ls = function()
+      require('lspconfig').ts_ls.setup({
+        settings = {
+          typescript = {
+            preferences = {
+              includePackageJsonAutoImports = "on"
+            }
+          },
+          javascript = {
+            preferences = {
+              includePackageJsonAutoImports = "on"
+            }
+          }
+        }
+      })
+    end,
   }
 })
 
--- You need to setup `cmp` after lsp-zero
+-- LSP keymaps
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(event)
+    local opts = { buffer = event.buf }
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts)
+  end,
+})
+
+-- Autocompletion
 local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
-
 cmp.setup({
-  mapping = {
-    -- `Enter` key to confirm completion
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
-
-    -- Ctrl+Space to trigger completion menu
+  sources = {
+    { name = 'nvim_lsp' },
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
     ['<C-Space>'] = cmp.mapping.complete(),
-
-    -- Navigate between snippet placeholder
-    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-  }
+  }),
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
 })
--- lua/grant/init.lua
+++ lua/grant/init.lua
@@ -1,2 +1,9 @@
-require("grant.remap")
+-- lua/grant/init.lua
+
+-- Load Python configuration first
+require("grant.python")
+
+-- Then load your other configurations
 require("grant.set")
+require("grant.packer")
+require("grant.remap")
